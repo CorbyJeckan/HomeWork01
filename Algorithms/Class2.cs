b29e3dc0ff4db8f947e2dba2718d270e69cbb8f2
Евгений Савельев <curtisjeckan@mail.ru>
2022-08-29 14:16:53 +0500

    Выполнил практическое задание к уроку 1

---
+++ Algorithms/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class Class2
+    {
+        public static void Function()
+        {
+            Console.WriteLine("Сложность функции: O(N^3)");
+        }
+        
+    }
+}
